f5ac669152e570d21a68d15d81dfa85b6de5de39
Eduardo Andreolli <andreolliedu77@gmail.com>
2022-09-07 20:12:31 -0300

    Initial commit

---
+++ projAutomoveis/Form3.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace projAutomoveis
+{
+    partial class frmcarro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmcarro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1342, 741);
+            this.Name = "frmcarro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastrar carro";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
